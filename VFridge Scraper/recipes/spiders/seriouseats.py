b7bc2ddf38b331b717c6514ae8935cf309a330a2
Joshua Johns <joshuajohns@wirelessprv-10-193-100-190.near.illinois.edu>
2022-10-21 11:18:20 -0500

    added functioning scraper

---
+++ VFridge Scraper/recipes/spiders/seriouseats.py
@@ -0,0 +1,37 @@
+import imp
+import scrapy
+import json
+
+
+class SeriouseatsSpider(scrapy.spiders.SitemapSpider):
+    name = 'seriouseats'
+    sitemap_urls = ['https://www.seriouseats.com/sitemap.xml']
+
+    def parse(self, response):
+
+        script = response.css('script[type="application/ld+json"]::text').get()
+        
+        if script is None:
+            return
+
+        temp = json.loads(script)
+
+        if temp[0]['@type'] != ['Recipe']:
+            return
+
+        ingredients = ""
+        idx = 0
+        print(type(temp[0]['recipeIngredient']))
+        for ingredient in temp[0]['recipeIngredient']:
+            idx += 1
+            ingredients += ingredient 
+            if (idx == (len(temp[0]['recipeIngredient']) - 1)):
+                break
+            ingredients += " -> "
+        print(ingredients)
+        
+        yield {
+            "url" : response.url,
+            "title" : temp[0]['headline'],
+            "ingredients" : ingredients,
+            }
