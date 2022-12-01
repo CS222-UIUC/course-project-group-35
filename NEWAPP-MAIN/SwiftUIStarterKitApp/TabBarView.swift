3f9718703ff30702366872895e75916a8271df4b
akashayyadevara <akash.sparr@gmail.com>
2022-12-01 17:55:50 -0600

    New app

---
+++ NEWAPP-MAIN/SwiftUIStarterKitApp/TabBarView.swift
@@ -0,0 +1,42 @@
+
+
+
+import SwiftUI
+
+struct TabbarView: View {
+    var body: some View {
+        TabView {
+            NavigationView {
+                ActivitiesContentView(activtiesData: Activities(data: ActivitiesMockStore.activityData, items: ActivitiesMockStore.activities))
+            }
+            .tag(0)
+            .tabItem {
+                Image("profile-glyph-icon")
+                    .resizable()
+                Text("Home")
+            }
+            
+            NavigationView {
+                ActivitiesCartView(ShoppingCartItemsData: ActivitiesCart(data: ActivitiesMockStore.shoppingCartData))
+            }
+            .tag(1)
+            .tabItem {
+                Image("shopping-cart-icon")
+                Text("Inventory")
+            }
+            
+            NavigationView {
+                     AccountView()
+                  }
+                   .tag(2)
+                    .tabItem {
+                    Image("profile-glyph-icon")
+                            .resizable()
+                    Text("Search")
+                }
+        }
+    }
+}
+
+
+
